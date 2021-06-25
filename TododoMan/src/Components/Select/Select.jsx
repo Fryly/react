14975b6f6e07ac3cad443e664e7d4265f65310be
Maxim <60447035+Fryly@users.noreply.github.com>
2021-06-25 17:22:32 +0300

    add component Select and style

---
+++ TododoMan/src/Components/Select/Select.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Select.css'
+
+const SELECT = ["Обычный", "Табличный"]
+
+function Select( {select, openSelect, handleClickSelect, handleOpenSelect} ) {
+
+    return (
+        <div className="Select-block">
+                <div className="Select-view">
+                    Вид: {select}
+                </div>
+                <div className="Select">
+                    <span 
+                        className="Select-text"
+                        onClick={handleOpenSelect}
+                    >
+                        {select}
+                    </span>
+                    {
+                        openSelect &&
+                            <div className="Option">
+                                {
+                                    SELECT.map( (item, index) => (
+                                        <span 
+                                            key={index}
+                                            onClick={ () => handleClickSelect(item)}
+                                        >
+                                            {item}
+                                        </span>
+                                    ))
+                                }
+                            </div>
+                    }
+                </div>
+        
+        </div>
+    )
+}
+
+export default Select
